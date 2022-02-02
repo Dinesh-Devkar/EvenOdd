97443aeca996353d4cda06d30ca3c16f95cf6ee9
Dinesh-Devkar <d.devkar386@gmail.com>
2022-02-02 11:33:05 +0530

    added comment in EveOddTest.java

--- src/com/techlab/evenodd/test/EvenOddTest.java
+++ src/com/techlab/evenodd/test/EvenOddTest.java
@@ -4,6 +4,7 @@ public class EvenOddTest {
 
 	public static void main(String[] args)
 	{
+		//java program to check whether the given number is even or odd
 		EvenOdd e=new EvenOdd();
 		e.isEven(50);
 	}
